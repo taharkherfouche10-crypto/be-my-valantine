fd2a6df7c5e49d8b55384923616e5127893d53de
taharkherfouche10-crypto <taharkherfouche10@gmail.com>
2026-01-29 12:35:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const messages = [
+    "Are you sure?",
+    "Really sure??",
+    "Are you positive?",
+    "kenzaaaaaaa please...",
+    "Just think about it!",
+    "If you say no, I will be really sad...",
+    "I will be very sad...",
+    "I will be very very very sad...",
+    "Ok fine, I will stop asking...",
+    "Just kidding, say yes please! ❤️"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
+const noButton = document.querySelector('.no-button');
+
+noButton.addEventListener("mouseover", () => {
+  const x = Math.random() * (window.innerWidth - 120);
+  const y = Math.random() * (window.innerHeight - 120);
+
+  noButton.style.position = "absolute";
+  noButton.style.left = x + "px";
+  noButton.style.top = y + "px";
+});
